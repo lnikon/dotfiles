2e639f6bd626f6f74628b6f82b9ce1a9eda961a5
lnikon <nreply@list.ru>
2019-12-29 17:38:17 +0400

    updated installation script

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-dir=~/.dotfiles
-olddir=~/dotfiles_old
+main_dotfiles_dir=~/.dotfiles
+old_dotfiles_dir=~/dotfiles_old
 files="bashrc tmux.conf vimrc zshrc vim oh-my-zsh"
 
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
+echo "Creating $old_dotfiles_dir for backup of any existing dotfiles in ~"
+mkdir -p $old_dotfiles_dir
 echo "...done"
 
-echo "Chainging to the $dir directory"
-cd $dir
+echo "Chainging to the $main_dotfiles_dir directory"
+cd $main_dotfiles_dir
 echo "...done"
 
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    echo "Moving any existing dotfiles from ~ to $old_dotfiles_dir"
+    mv ~/.$file $old_dotfiles_dir
     echo "Creating symlink to $file in home directory"
-    ln -s $dir/$file ~/.$file
+    ln -s $main_dotfiles_dir/$file ~/.$file
 done
