5933bfbf54754f54f3d97200eea539dcf62a8d05
Vahag Bejanyan <vbejanyan@questrade.com>
2024-02-05 15:07:15 +0400

    update telescope configs

--- nvim/.config/nvim/lua/vagag/telescope.lua
+++ nvim/.config/nvim/lua/vagag/telescope.lua
@@ -11,11 +11,7 @@ telescope.setup({
 -- Configure mappings
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({search = vim.fn.input("Grep > ");})
-end)
-
-vim.keymap.set('n', '<leader>fss', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>fsl', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>gbr', builtin.git_branches, {})
 vim.keymap.set('n', '<leader>trs', builtin.treesitter, {})
