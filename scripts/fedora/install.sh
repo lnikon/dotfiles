179db7394597f9621f80c0fe26444cce2c1db70e
lnikon <bejanyan.vahag@protonmail.com>
2024-11-04 15:36:37 +0400

    Add installation packages into fedora

--- scripts/fedora/install.sh
+++ scripts/fedora/install.sh
@@ -9,6 +9,9 @@ sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-releas
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
+# Install nordvpn
+sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
+
 # Update the system
 sudo dnf check-update
 sudo dnf update --refresh -y
